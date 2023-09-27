48a29daeb3d99e28fa826094c306b0e7ca633b29
Abdullah Omar <abdullahomar.dev@gmail.com>
2023-09-27 12:12:10 -0700

    develop collapsible component

---
+++ src/components/collapsible.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="collapsible mb-5">
+        <h3  @click="toggleCollapsible" class="text-lg px-4" style="cursor: pointer;">
+            <span>{{ title }}</span>
+            <v-icon icon="mdi-chevron-up" v-if="isOpen"></v-icon>
+            <v-icon icon="mdi-chevron-down" v-if="!isOpen"></v-icon>
+        </h3>
+        <div v-if="isOpen" >
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps(['title'])
+const isOpen = ref(false)
+
+const toggleCollapsible = (e:any) => {
+    isOpen.value = !isOpen.value;
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
