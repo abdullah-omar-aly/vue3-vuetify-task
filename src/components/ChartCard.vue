2fd042d992eb30961aaf42950cc71bc1377ee3c7
Abdullah Omar <abdullahomar.dev@gmail.com>
2023-09-28 12:40:18 -0700

    adding two charts with chartjs and vue-chartjs

---
+++ src/components/ChartCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-col cols="12" sm="12" md="6"  lg="4" >
+        <v-sheet class="ma-5 rounded-lg elevation-3" height="100%">
+            <h5 style="border-bottom: 1px solid lightgray; " class="pa-4 mb-10">
+                <v-icon :icon="icon.name" class="rounded-lg px-7 py-5" style="color:white;width: 50px;"
+                    :style="icon.style"></v-icon>
+                <span class="mr-4">{{ title }}</span>
+            </h5>
+            <div>
+                <slot></slot>
+            </div>
+        </v-sheet>
+    </v-col>
+</template>
+
+<script setup lang="ts">
+
+defineProps(['title', 'icon'])
+
+</script>
+
+<style scoped lang="scss"></style>
--- src/sections/information-board-section.vue
+++ src/sections/information-board-section.vue
@@ -2,72 +2,170 @@
     <Collapsible title="لوحة المعلومات">
 
         <v-row no-gutters>
-            <v-col cols="12" sm="6" md="4">
-                <v-sheet class="ma-4 rounded-lg elevation-3" style="overflow: hidden;min-height: 300px;">
+
+            <ChartCard title="مؤشرات المهام" :icon="{
+                name: 'mdi-checkbox-marked-circle-outline',
+                style: 'background-color: #969FB0;'
+            }">
+                <div style="display: flex;padding: 2rem;justify-content: center;gap: 5%;">
                     <div>
-                        <h5 style="border-bottom: 1px solid lightgray; " class="pa-4 mb-10">
-                            <v-icon icon="mdi-checkbox-marked-circle-outline"
-                                style="color: white; background-color: lightgray;width: 50px;"
-                                class="rounded px-5 py-4"></v-icon>
-                            <span class="mr-4">مؤشرات المهام</span>
-                        </h5>
-                        <!-- piechart -->
-                        <div :style="pieStyle">
-                            <div style="height: 110px;width: 110px;background-color: white;border-radius: 50%;"></div>
+                        <Doughnut style="width: 170px; max-height: 170px;margin: 0 auto;" :options="TasksChartCofig.options"
+                            :data="TasksChartCofig.data" />
+                    </div>
+                    <!--custom Legend -->
+                    <div style="flex-direction: column;  justify-content: center;flex-grow: 1;gap: 5px;">
+                        <div v-for="(indicator, index) in TaskIndicators" :key="`legend-badge-${index}`"
+                            style="display: flex;align-items: center;gap: 10px;">
+                            <div>
+                                <span :style="{ 'background-color': indicator.color }" class="badge"></span>
+                            </div>
+                            <div>
+                                <p style="font-weight: bold;">{{ indicator.value }}</p>
+                                <p style="font-size: 12px;">{{ indicator.label }}</p>
+                            </div>
                         </div>
                     </div>
-                </v-sheet>
-            </v-col>
+                </div>
 
-            <v-col cols="12" sm="6" md="4">
-                <v-sheet class="ma-4 rounded-lg elevation-3" style="overflow: hidden;min-height: 300px;">
-                    <div>
+            </ChartCard>
 
-                        <h5 style="border-bottom: 1px solid lightgray; " class="pa-4 mb-10">
-                            <v-icon icon="mdi-checkbox-marked-circle-outline"
-                                style="color: white; background-color: lightgray;width: 50px;"
-                                class="rounded px-5 py-4"></v-icon>
-                            <span class="mr-4">مؤشرات المهام</span>
-                        </h5>
-                        <!-- halfpie chart -->
-                        <div :style="halfPieStyle">
-                            <div style="height: 140px;width: 140px;background-color: white;border-radius: 50%;"></div>
+            <ChartCard title="مؤشرات الاجتماعات" :icon="{
+                name: 'mdi-calendar-blank',
+                style: 'background-color: #8A55A6'
+            }">
+                <div class="px-10">
+
+                    <Doughnut style="width: 200px; max-height: 200px;margin: 0 auto;" :options="MeetingsChartConfig.options"
+                        :data="MeetingsChartConfig.data" />
+                    <!-- custom Legend -->
+                    <div class="py-10"
+                        style="display:flex; justify-content: center;flex-grow: 1;flex-direction: row;gap: 10%;">
+                        <div v-for="(indicator, index) in MeetingIndicators" :key="`legend-badge-${index}`"
+                            style="display: flex;align-items: center;gap: 10px;">
+                            <div>
+                                <span :style="{ 'background-color': indicator.color }" class="badge"></span>
+                            </div>
+                            <div>
+                                <p style="font-weight: bold;">{{ indicator.value }}</p>
+                                <p style="font-size: 12px;">{{ indicator.label }}</p>
+                            </div>
                         </div>
 
                     </div>
-                </v-sheet>
-            </v-col>
+                </div>
+
+
+            </ChartCard>
+
+
+
         </v-row>
     </Collapsible>
 </template>
 
 <script setup lang="ts">
 import Collapsible from '../components/collapsible.vue';
+import ChartCard from '@/components/ChartCard.vue';
+import { Chart as ChartJS, ArcElement, Tooltip, Title, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { computed } from 'vue';
 import { reactive } from 'vue';
+import { Doughnut, Bar } from 'vue-chartjs'
 
-const pieStyle = reactive({
-    display: "grid",
-    placeContent: "center",
-    width: "160px",
-    height: "160px",
-    borderRadius: "50%",
-    backgroundColor: "blueviolet",
-    backgroundImage: "repeating-conic-gradient(red 0deg 90deg, yellow 90deg 200deg, blue 200deg 360deg)",
-    transform: "rotateZ(-90deg)",
-    margin: "auto"
-})
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, ArcElement)
 
-const halfPieStyle = reactive({
-    display: "grid",
-    placeContent: "center",
-    width: "200px",
-    height: "200px",
-    borderRadius: "50%",
-    backgroundColor: "blueviolet",
-    backgroundImage: "repeating-conic-gradient(red 0deg 90deg, yellow 90deg 120deg, blue 120deg 180deg , white 180deg 360deg)",
-    transform: "rotateZ(-90deg)",
-    margin: "auto"
-})
+
+const TaskIndicators = reactive([
+    {
+        value: 28,
+        label: 'اكتملت في الموعد',
+        color: '#0FB96A'
+    },
+    {
+        value: 4,
+        label: 'تجاوزت الموعد',
+        color: '#8A55A6'
+    },
+    {
+        value: 12,
+        label: 'مجدولة',
+        color: '#D0D4DD'
+    },
+])
+
+const MeetingIndicators = reactive([
+    {
+        value: 28,
+        label: 'اكتملت',
+        color: '#0FB96A'
+    },
+    {
+        value: 4,
+        label: 'مجدولة',
+        color: '#D0D4DD'
+    },
+    {
+        value: 12,
+        label: 'لم تكتمل',
+        color: '#8A55A6'
+    },
+])
+
+
+const TasksChartCofig = computed(() => ({
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        cutout: 60, // Display half of the pie
+        elements: {
+            arc: {
+                borderWidth: 0,        // Set border width to 0
+                borderColor: 'transparent', // Set border color to transparent
+            },
+        },
+    },
+    data: {
+        labels: TaskIndicators.map(indicator => indicator.label),
+        datasets: [
+            {
+                backgroundColor: TaskIndicators.map(indicator => indicator.color),
+                data: TaskIndicators.map(indicator => indicator.value),
+            }
+        ]
+    }
+}))
+const MeetingsChartConfig = computed(() => ({
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        cutout: 80, // Display half of the pie
+        rotation: -90, // Rotate by 180 degrees (half-pie)
+        circumference: 180,
+        elements: {
+            arc: {
+                borderWidth: 0, // Set border width to 0
+                borderColor: 'transparent', // Set border color to transparent
+            },
+        },
+    },
+    data: {
+        labels: MeetingIndicators.map(indicator => indicator.label),
+        datasets: [
+            {
+                backgroundColor: MeetingIndicators.map(indicator => indicator.color),
+                data: MeetingIndicators.map(indicator => indicator.value)
+            }
+        ]
+    }
+}))
 </script>
 
-<style scoped></style>
+<style scoped>
+.badge {
+    border-radius: 50%;
+    padding: 0;
+    width: 12px;
+    height: 12px;
+    display: inline-block;
+    margin-right: 3px;
+}
+</style>
