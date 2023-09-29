28157660f6743161048502a75a541a0002a22b05
Abdullah Omar <abdullahomar.dev@gmail.com>
2023-09-29 14:41:38 -0700

    refactor members section (extract inline styles - extract dummy data for separate file) - adding a dialog to show all members

--- src/sections/MembersSection.vue
+++ src/sections/MembersSection.vue
@@ -1,35 +1,50 @@
 <template>
     <Collapsible title="الأعضاء">
         <div style="display: flex;">
-            <ul ref="container" class="d-flex"
-                style="height: 150px ;list-style: none;flex-grow: 1 ;flex-wrap: wrap;overflow: hidden;">
+            <ul ref="container" class="cards-container" style=";">
+                <!-- member card -->
                 <template v-for="member in members" :key="member.id">
-
-                    <!-- card -->
                     <v-hover v-slot="{ isHovering, props }">
                         <v-card tag="div" :style="isHovering ? 'background-color: white' : 'background-color: transparent'"
-                            style="flex-grow:1;min-width:115px;height: calc(100% - 10px);margin-bottom: 30px ;cursor: pointer;padding:1.4rem ;"
-                            :elevation="isHovering ? 2 : 0" class="rounded-lg  text-center"
-                            :class="{ 'on-hover': isHovering }" v-bind="props">
-                            <v-avatar icon="$vuetify" style="border: 3px solid lightgray;" class="border-gray-500"
-                                image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPLus1qayz4hD9hJ8xxZBaIjEkrm62BNiS5g&usqp=CAU"
+                            :elevation="isHovering ? 2 : 0" class="member-card" :class="{ 'on-hover': isHovering }"
+                            v-bind="props">
+                            <v-avatar icon="$vuetify" class="member-card__avatar" :image="member.avatar"
                                 size="50"></v-avatar>
-                            <p style="font-weight:bolder;font-size: 14px;margin-top: 0.5rem;">{{ member.name }}</p>
-                            <p class="text-xs" style="font-size: 12px;">{{ member.title }}</p>
+                            <p class="member-card__name">{{ member.name }}</p>
+                            <p class="member-card__title">{{ member.title }}</p>
                         </v-card>
                     </v-hover>
                 </template>
             </ul>
-            <div class="hidden-cards" style="min-width: 115px; padding-top: 20px;" v-if="hiddenCardCount > 0">
 
-                <button style="width: 100%;cursor: pointer;">
-                    <div
-                        style="border: 3px solid lightgray;background-color: gray;color: white;border-radius: 50%;width: 50px;height: 50px;display: grid;place-content: center;margin: auto;">
-                        +{{ hiddenCardCount }}
-                    </div>
-                    <p style="font-weight:bolder;font-size: 16px;margin-top: 0.5rem;">الكل</p>
-                </button>
-            </div>
+            <v-dialog v-model="dialog" scrollable width="100%" max-width="500px">
+                <template v-slot:activator="{ props }">
+                    <button class="hidden-members-btn" v-bind="props" v-if="hiddenCardCount > 0">
+                        <button class="hidden-members-btn__count">
+                            +{{ hiddenCardCount }}
+                        </button>
+                        <p class="hidden-members-btn__label">الكل</p>
+
+                    </button>
+                </template>
+                <v-card class="all-members-dialog">
+                    <v-card-title>كل الأعضاء</v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        <div v-for="member in members" :key="member.id" class="all-members-dialog__body">
+                            <h4>{{ member.name }}</h4>
+                            <v-avatar icon="$vuetify" :image="member.avatar" size="50"
+                                style="border: 3px solid gray;"></v-avatar>
+                        </div>
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                            اغلق
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </div>
     </Collapsible>
 </template>
@@ -39,33 +54,13 @@ import Collapsible from '../components/Collapsible.vue';
 import { onBeforeUnmount } from 'vue';
 import { ref, onMounted } from 'vue';
 import { reactive } from 'vue';
+import { membersInfo } from '../data/membersData'
+const dialog = ref(false)
 
 
-const initialValue = [
-    { id: '1', name: "اسم العضو", title: "الرئيس" },
-    { id: '2', name: "اسم العضو", title: "نائب الرئيس" },
-    { id: '3', name: "اسم العضو", title: "" },
-    { id: '4', name: "اسم العضو", title: "" },
-    { id: '5', name: "اسم العضو", title: "" },
-    { id: '6', name: "اسم العضو", title: "" },
-    { id: '7', name: "اسم العضو", title: "" },
-    { id: '8', name: "اسم العضو", title: "" },
-    { id: '9', name: "اسم العضو", title: "" },
-    { id: '10', name: "اسم العضو", title: "" },
-    { id: '11', name: "اسم العضو", title: "" },
-    { id: '12', name: "اسم العضو", title: "" },
-    { id: '13', name: "اسم العضو", title: "" },
-    { id: '14', name: "اسم العضو", title: "" },
-    { id: '15', name: "اسم العضو", title: "" },
-    { id: '16', name: "اسم العضو", title: "" },
-    { id: '17', name: "اسم العضو", title: "" },
-    { id: '18', name: "اسم العضو", title: "" },
-    { id: '19', name: "اسم العضو", title: "" },
-    { id: '20', name: "اسم العضو", title: "" },
-]
 const container = ref(null)
 const hiddenCardCount = ref(0);
-const members = reactive(initialValue)
+const members = reactive(membersInfo)
 
 const updateHiddenCardsCount = () => {
 
@@ -92,4 +87,79 @@ onBeforeUnmount(() => {
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.cards-container {
+    display: flex;
+    height: 150px;
+    list-style: none;
+    flex-grow: 1;
+    flex-wrap: wrap;
+    overflow: hidden
+}
+
+.member-card {
+    flex-grow: 1;
+    min-width: 115px;
+    height: calc(100% - 10px);
+    margin-bottom: 30px;
+    cursor: pointer;
+    padding: 1.4rem;
+    text-align: center;
+    border-radius: var(--rounded-primary);
+
+    &__avatar {
+        border: 3px solid lightgray;
+    }
+
+    &__name {
+        font-weight: bolder;
+        font-size: 14px;
+        margin-top: 0.5rem;
+    }
+
+    &__title {
+        font-size: 12px;
+    }
+}
+
+.hidden-members-btn {
+    flex-grow: 1;
+    min-width: 115px;
+    height: calc(100% - 10px);
+    margin-bottom: 30px;
+    padding: 1.4rem;
+    text-align: center;
+    border-radius: var(--rounded-primary);
+
+    &__count {
+        border: 3px solid lightgray;
+        background-color: gray;
+        color: white;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        display: grid;
+        place-content: center;
+        margin: auto;
+    }
+
+    &__label {
+        font-weight: bolder;
+        font-size: 16px;
+        margin-top: 0.5rem
+    }
+}
+
+.all-members-dialog {
+    text-align: right;
+    border-radius: var(--rounded-primary);
+
+    &__body {
+        display: flex;
+        gap: 1rem;
+        justify-content: end;
+        align-items: center;
+        padding-bottom: 0.8rem;
+    }
+}
+</style>
